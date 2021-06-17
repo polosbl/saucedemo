ea4073dcab25cf716d60fe9f92c1a117c63be15f
Alexander Gordeichik <a.gordeichik@invento.by>
2021-06-17 14:25:48 +0300

    soucedemo fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
@@ -32,20 +33,20 @@ public class BaseTest implements ITestConstants {
     public void initTest(ITestContext context){
         //TODO: Different browsers (Firefox)
 //        if (System.getProperty("browser").equals("chrome")) {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("start-maximized"); // open Browser in maximized mode
 //        options.addArguments("disable-infobars"); // disabling infobars
 //        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
 //        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
+//        options.addArguments("--no-sandbox"); // Bypass OS security model
 //      DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 //      desiredCapabilities.setCapability("nativeWebScreenshot",true);
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
 //        } else if (System.getProperty("browser").equals("firefox")){
-//            WebDriverManager.firefoxdriver().setup();
-//            driver = new FirefoxDriver();
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
 //        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
