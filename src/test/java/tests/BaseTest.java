0eef48d1b759310b29ef11130409aa386f0df44e
Alexander Gordeichik <a.gordeichik@invento.by>
2021-06-17 14:39:41 +0300

    soucedemo fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -36,13 +36,13 @@ public class BaseTest implements ITestConstants {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
-//        options.addArguments("--headless");
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--headless");
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 //      DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 //      desiredCapabilities.setCapability("nativeWebScreenshot",true);
         driver = new ChromeDriver(options);
