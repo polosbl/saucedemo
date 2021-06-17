c5abcfad3066c4dac9710a3bf6ee4292d03bb777
Alexander Gordeichik <a.gordeichik@invento.by>
2021-06-17 14:38:51 +0300

    soucedemo fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -34,16 +34,18 @@ public class BaseTest implements ITestConstants {
         //TODO: Different browsers (Firefox)
 //        if (System.getProperty("browser").equals("chrome")) {
         WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+//        options.addArguments("--headless");
+//        options.setExperimentalOption("useAutomationExtension", false);
 //        options.addArguments("start-maximized"); // open Browser in maximized mode
 //        options.addArguments("disable-infobars"); // disabling infobars
 //        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
 //        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--no-sandbox"); // Bypass OS security model
 //      DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 //      desiredCapabilities.setCapability("nativeWebScreenshot",true);
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
 //        } else if (System.getProperty("browser").equals("firefox")){
 //            WebDriverManager.firefoxdriver().setup();
 //            driver = new FirefoxDriver();
