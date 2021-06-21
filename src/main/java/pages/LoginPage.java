2e19ce2936502619d0245f68e78a736227cc99fa
Alexander Gordeichik <a.gordeichik@invento.by>
2021-06-21 23:31:02 +0300

    Homework 20: Logs

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
+@Log4j2
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
@@ -40,35 +42,45 @@ public class LoginPage extends BasePage {
 
     @Step("Logging in as standard user")
     public ProductsPage login(String username, String password) {
+        log.info(String.format("Filling in username: '%s' in login field",username));
         usernameInput.sendKeys(username);
+        log.info(String.format("Filling in username: '%s' in login field",password));
         passwordInput.sendKeys(password);
+        log.info("Clicking Login button.");
         loginButton.click();
         return new ProductsPage(driver);
     }
 
     @Step("Logging in with incorrect credentials")
     public LoginPage loginWithError(String username, String password) {
+        log.info(String.format("Filling in username: '%s' in login field",username));
         usernameInput.sendKeys(username);
+        log.info(String.format("Filling in username: '%s' in login field",password));
         passwordInput.sendKeys(password);
+        log.info("Clicking Login button.");
         loginButton.click();
         return this;
     }
 
     public String getErrorMessageText() {
+        log.info("Getting error message text.");
         return errorMessage.getText();
     }
 
     public boolean isErrorMessagePresent() {
+        log.info("Checking is error message present on page");
         return errorMessages.isEmpty();
     }
 
     @Step("Closing error message")
     public void closeErrorMessage() {
+        log.info("Closing error message by clicking on X-button");
         closeErrorMessageButton.click();
     }
 
     @Step ("Opening Login page")
     public LoginPage openPage(String url) {
+        log.info("Open Login page, URL: " + url);
         driver.get(url);
         return this;
     }
